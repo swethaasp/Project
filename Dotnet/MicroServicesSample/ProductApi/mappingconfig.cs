2295b2359b3da5dd42d095c031155693430e5b0c
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-10-28 14:17:35 +0530

    Add files via upload

---
+++ Dotnet/MicroServicesSample/ProductApi/mappingconfig.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+
+using MicroServicesSample.Services.ProductAPI.Models;
+
+using MicroServicesSample.Services.ProductAPI.Models.Dto;
+
+namespace MicroServiceSample.Services.ProductAPI
+
+{
+
+    public class MappingConfig
+
+    {
+
+        public static MapperConfiguration RegisterMaps()
+
+        {
+
+            var mappingConfig = new MapperConfiguration(config =>
+
+            {
+
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+
+            });
+
+            return mappingConfig;
+
+        }
+
+    }
+
+}
