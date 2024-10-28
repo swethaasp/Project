6510e058087324d6ce06b919feb4595ed104f95e
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-10-28 13:01:01 +0530

    Add files via upload

---
+++ Dotnet/MicroServicesSample/AuthApi/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using MicroServicesSample.Services.AuthAPI.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using MicroServiceSample.Services.AuthAPI.Models;
+
+namespace MicroServicesSample.Services.AuthAPI.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+         
+        }
+
+      public DbSet<ApplicationUser> ApplicationUsers {  get; set; }     
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+        }
+        
+    }
+
+}
+
