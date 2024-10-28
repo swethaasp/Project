f08d09064e3816b10d30292cb7bd542897fdff5c
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-10-28 10:49:01 +0530

    Add files via upload

---
+++ Angular/my-first-project/src/app/components/add employee/add-employee.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-add-employee',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-employee.component.html',
+  styleUrl: './add-employee.component.css'
+})
+export class AddEmployeeComponent {
+
+}
