6af37e1c3ad25e7bbfc36fa1b2ff66fc1a7d52fa
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-10-28 14:05:09 +0530

    Add files via upload

---
+++ Dotnet/MicroServicesSample/AuthApi/Services/IService/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using MicroServiceSample.Services.AuthAPI.Models.Dto;
+
+namespace MicroServiceSample.Services.AuthAPI.Service.IService
+{
+    public interface IAuthService
+    {
+       Task<string> Register(RegistrationRequestDto registrationRequestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+
+        Task<bool> AssignRole(string email, string roleName);
+    }
+}
---
+++ Dotnet/MicroServicesSample/AuthApi/Services/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using MicroServiceSample.Services.AuthAPI.Models;
+
+namespace MicroServiceSample.Services.AuthAPI.Service.IService
+{
+    public interface IJwtTokenGenerator
+    {
+     string GenerateToken(ApplicationUser applicationUser);
+    }
+}
