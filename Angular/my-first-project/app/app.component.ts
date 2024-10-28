57aff7627003ed718eaacc3a859c8c1244c4c7f3
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-10-28 10:44:05 +0530

    Add files via upload

---
+++ Angular/my-first-project/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { DataBindingComponent } from './components/data-binding/data-binding.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,AddEmployeeComponent,EmployeeListComponent, DataBindingComponent],
+
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'my-first-project';
+  name='Swethaaaaa';
+ 
+
+}
