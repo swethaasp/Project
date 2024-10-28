d44fc89308017f17ee62dd789d1793bb268a1eab
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-10-28 12:56:43 +0530

    Add files via upload

---
+++ Dotnet/Automapper/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace Automapper.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        // Sensitive Information
+        public string SocialSecurityNumber { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
+
