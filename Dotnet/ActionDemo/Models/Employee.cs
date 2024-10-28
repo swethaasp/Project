276e43462958b01073b579e27ea85e7d51b3ad06
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-10-28 12:51:34 +0530

    Add files via upload

---
+++ Dotnet/ActionDemo/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ActionDemo.Models
+{
+    public class Employee
+    {
+        public static int Count { get; internal set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public int Age { get; set; }
+        public string Department { get; set; }
+
+       
+    }
+}
