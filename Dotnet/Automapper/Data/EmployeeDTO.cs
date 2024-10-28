f912f34b2cfd8ac431ed8629cf4a6a78d6219b34
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-10-28 12:55:43 +0530

    Add files via upload

---
+++ Dotnet/Automapper/Data/EmployeeDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Automapper.Data
+{
+    public class EmployeeDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+    }
+}
